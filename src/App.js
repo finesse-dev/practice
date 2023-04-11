c46333921571c2b6daf2e53ea7862778e87b5abf
dev-geeks <gurpreets1597@gmail.com>
2023-04-11 20:21:08 +0530

    Started with Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Routes, Link } from "react-router-dom";
 import './App.css';
+import About from "./routers/About";
+import Contact from "./routers/Contact";
 
 function App() {
   return (
-    <h1>hi</h1>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <About/> } />
+        <Route path="/contact" element={ <Contact/> } />
+      </Routes>
+    </div>
   );
 }
 
--- src/routers/About.js
+++ src/routers/About.js
@@ -1,7 +1,6 @@
 import React from "react";
-
 const About = () =>{
-    return(<div></div>)
+    return(<div>About Page</div>)
 }
 
 export default About;
