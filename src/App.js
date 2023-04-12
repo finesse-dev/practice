919b4198f5bc1b8fd8647f5ad3f7b3091e7b0017
dev-geeks <gurpreets1597@gmail.com>
2023-04-12 21:54:00 +0530

    Created 404 page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import About from "./routers/About";
 import Contact from "./routers/Contact";
 import NewContact from "./routers/NewContact";
-
+import NotFound from "./routers/NotFound";
 function App() {
   return (
     <div>
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={ <About/> } />
         <Route path="/contact" element={ <Contact/> } />
         <Route path="/contact/:id" element={<NewContact/>}/>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </div>
   );
