89b3eb7c9e984ea2513e278c5a628fd4f26c8dca
dev-geeks <gurpreets1597@gmail.com>
2023-05-11 19:04:51 +0530

    Added navbar

--- src/newrouters/Home.jsx
+++ src/newrouters/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Home = () =>{
     return(
         <div>
+            <header>
+                <a href="#">Logo</a>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/about">About</NavLink></li>
+                        <li><NavLink to="/blog">Blog</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
             <h1>HOME PAGE</h1>
         </div>
     )
