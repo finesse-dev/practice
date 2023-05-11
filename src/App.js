eb357e9f91ec144fb305de873110910e08f9e318
dev-geeks <gurpreets1597@gmail.com>
2023-05-11 18:43:47 +0530

    Routing new Home page

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import About from "./routers/About";
 import Contact from "./routers/Contact";
 import NewContact from "./routers/NewContact";
 import NotFound from "./routers/NotFound";
+
+import Home from "./newrouters/Home";
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={ <About/> } />
-        <Route path="/contact">
-          <Route index element={<Contact/>}/>
-          <Route path=":id" element={<NewContact/>}/>
-        </Route>
-        {/* <Route path="/contact/:id" element={<NewContact/>}/> */}
-        <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <nav>
+  //       <ul>
+  //         <li><Link to="/">About</Link></li>
+  //         <li><Link to="/contact">Contact</Link></li>
+  //       </ul>
+  //     </nav>
+  //     <Routes>
+  //       <Route path="/" element={ <About/> } />
+  //       <Route path="/contact">
+  //         <Route index element={<Contact/>}/>
+  //         <Route path=":id" element={<NewContact/>}/>
+  //       </Route>
+  //       {/* <Route path="/contact/:id" element={<NewContact/>}/> */}
+  //       <Route path="*" element={<NotFound/>}></Route>
+  //     </Routes>
+  //   </div>
+  // );
+
+  return(
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  )
 }
 
 export default App;
